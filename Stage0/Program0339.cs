508433818275d97456c8dd085c09bba239fe68bc
MoriaRoz <ruznurhv@gmail.com>
2023-12-26 10:40:30 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program0339.cs
@@ -0,0 +1,19 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome0339();
+            Welcome9553();
+        }
+        static partial void Welcome9553();
+
+        private static void Welcome0339()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to  my console application", name);
+        }
+    }
+}
