ce7ca92ad595f0c909b7e3257173fe6bc9673051
einatma <einatmazuz@gmail.com>
2024-01-04 18:59:32 +0200

    Chapter 5 - Creating the database uCreating lists in the DalList project

---
+++ DalList/DataSource.cs
@@ -0,0 +1,17 @@
+﻿namespace Dal;
+internal static class DataSource
+{
+    internal static class Config
+    {
+        internal const int startDependencyId = 0;
+        private static int nextDependencyId = startDependencyId;
+        internal static int nextDependencyId { get => nextDependencyId++; }
+        internal const int startTaskId = 0;
+        private static int nextTaskId = startTaskId;
+        internal static int nextTaskId { get => nextTaskId++; }
+    }
+    internal static List<DO.Engineer> Engineers { get; } = new();
+    internal static List<DO.Dependency> Dependencys { get; } = new();
+    internal static List<DO.Task> Tasks { get; } = new();
+
+}
