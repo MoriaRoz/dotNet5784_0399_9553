b5e1cefcfff9879fcf4c918ce81861848bc2ba32
MoriaRoz <ruznurhv@gmail.com>
2024-01-07 03:29:57 +0200

    Part 8c-d and beginning of 9

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -1,40 +1,154 @@
 ﻿namespace DalTest;
 using DalApi;
 using DO;
-public static class Initialization
-    {
-        private static IEngineer? s_dalEngineer; //stage 1
-        private static IDependency? s_dalDependency; //stage 1
-        private static ITask? s_dalTask; //stage 1
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq.Expressions;
 
-        private static readonly Random s_rand = new();
-        private static void createSEngineer()
+public static class Initialization
+{
+    private static IEngineer? s_dalEngineer; //stage 1
+    private static IDependency? s_dalDependency; //stage 1
+    private static ITask? s_dalTask; //stage 1
+
+    private static readonly Random s_rand = new();
+    private static void createEngineer()
+    {
+        string[] engineerNames =
         {
-            string[] engineerNames =
-            {
             "Dani Levi", "Eli Amar", "Yair Cohen",
             "Ariela Levin", "Dina Klein", "Shira Israelof"
         };
 
-            foreach (var _name in engineerNames)
+        foreach (var _name in engineerNames)
+        {
+            Random rnd = new Random();
+
+            int _id;
+            do
+                _id = rnd.Next(200000000, 400000000);
+            while (s_dalEngineer?.Read(_id) != null);
+
+            string? _email = _name + "@gamil.com";
+            _email.Replace(' ', '_');
+
+            double _cost = 100.0;
+
+            LevelEngineer _level = LevelEngineer.Beginner;
+            int _levelNum = rnd.Next(0, 5);
+            switch (_levelNum)
             {
-                int _id;
-                do
-                    _id = s_rand.Next(MIN_ID, MAX_ID);
-                while (s_dalStudent!.Read(_id) != null);
-
-
-                DateTime start = new DateTime(1995, 1, 1);
-                int range = (DateTime.Today - start).Days;
-                DateTime _bdt = start.AddDays(s_rand.Next(range));
-
-                string? _alias = (_id % 2) == 0 ? _name + "ALIAS" : null;
-
-                Student newStu = new(_id, _name, _alias, _b, _bdt);
-
-                s_dalStudent!.Create(newStu);
+                case 0:
+                    _level = LevelEngineer.Beginner;
+                    _cost = 17.133;
+                    break;
+                case 1:
+                    _level = LevelEngineer.AdvancedBeginner;
+                    _cost = 200.355;
+                    break;
+                case 2:
+                    _level = LevelEngineer.Intermediate;
+                    _cost = 283.566;
+                    break;
+                case 3:
+                    _level = LevelEngineer.Advanced;
+                    _cost = 366.783;
+                    break;
+                case 4:
+                    _level = LevelEngineer.Expert;
+                    _cost = 450.00;
+                    break;
             }
-        }
 
+            Engineer newEng = new(_id, _name, _email, _level, _cost);
+            s_dalEngineer!.Create(newEng);
+        }
+    }
+    private static void createTask()
+    {
+        int i = 0;
+        Random rnd = new Random();
+
+        string[] nameTasks =
+        {
+            "","",""
+        };
+        string[] descripTasks =
+        {
+            "","",""
+        };
+        foreach (string _name in nameTasks)
+        {
+            string? _description = descripTasks[i];
+            
+            DateTime _createdAtDate = DateTime.Now;
+            DateTime? _deadlineDate = _createdAtDate.AddDays(rnd.Next(1, 60));
+
+            TimeSpan? _requiredEffortTime=_deadlineDate-_createdAtDate;
+            
+            bool _isMilestone = false;
+
+            LevelEngineer _complexity = LevelEngineer.Beginner;
+            int _compNum = rnd.Next(0, 5);
+            switch (_compNum)
+            {
+                case 0:
+                    _complexity = LevelEngineer.Beginner;
+                    break;
+                case 1:
+                    _complexity = LevelEngineer.AdvancedBeginner;
+                    break;
+                case 2:
+                    _complexity = LevelEngineer.Intermediate;
+                    break;
+                case 3:
+                    _complexity = LevelEngineer.Advanced;
+                    break;
+                case 4:
+                    _complexity = LevelEngineer.Expert;
+                    break;
+            }
+
+            DateTime? _startDate = null;
+            
+            DateTime? _completeDate = null;
+            
+            string? _deliverables = null;
+            
+            string? _remarks = null;
+
+            int _engineerId;
+            Engineer? en;
+            do
+            {
+                _engineerId = rnd.Next(200000000, 400000000);
+                en = s_dalEngineer?.Read(_engineerId);
+            }
+            while (en != null && en.Level >= _complexity);
+
+            Task newTa = new Task(0,_name,_description,_createdAtDate,
+                _requiredEffortTime,_isMilestone,_complexity,_startDate,
+                _deadlineDate,_completeDate,_deliverables,_remarks,_engineerId);
+            s_dalTask!.Create(newTa);
+            i++;
+        }
+    }
+    private static void createDependency()
+    {
+        for (int i = 1; i <= 40; i++)
+        {
+
+        }
+    }
+
+    public static void Do(IEngineer? engineer, ITask? task, IDependency dependency)
+    {
+        s_dalEngineer = engineer ?? throw new NullReferenceException("DAL can not be null!");
+        s_dalTask = task ?? throw new NullReferenceException("DAL can not be null!");
+        s_dalDependency = dependency ?? throw new NullReferenceException("DAL can not be null!");
+        createEngineer();
+        createTask();
+        createDependency();
+    }
 }
 
--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace DalTest
+﻿using Dal;
+using DalApi;
+
+namespace DalTest
 {
     internal class Program
     {
+        private static IEngineer? s_dalEngineer = new EngineerImplementation();
+        private static ITask? s_dalTask = new TaskImplementation();
+        private static IDependency? s_dalDependency = new DependencyImplementation();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Initialization.Do(s_dalEngineer,s_dalTask,s_dalDependency);
+            Console.WriteLine("Select entity you want to check-\r\n" +
+                "0- Exit\r\n" +
+                "1- Engineer\r\n" +
+                "2- task\r\n" +
+                "3- dependence");
+            int entity=int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Select the method you want to perform-\r\n" +
+                "0- exit from main menu\r\n" +
+                "1- Adding a new object of the entity type to the list (Create)\r\n" +
+                "2- Object display by ID (Read)\r\n" +
+                "3- Display the list of all objects of the entity type (ReadAll)\r\n" +
+                "4- Update existing object data (Update)\r\n" +
+                "5- Deleting an existing object from a list. (delete)\r\n" +
+                "     Please note: this option is only available for some entities");
+            int action = int.Parse(Console.ReadLine());
         }
     }
+
 }
