bf05d398982f95a5e2faf4478d497451b4968bd7
einatma <einatmazuz@gmail.com>
2024-02-15 19:01:12 +0200

    stage 4 part 6

---
+++ PL/Enums.cs
@@ -0,0 +1,9 @@
+﻿namespace PL;
+internal class LevelEngineerCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.LevelEngineer> s_enums =
+(Enum.GetValues(typeof(BO.LevelEngineer)) as IEnumerable<BO.LevelEngineer>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+
