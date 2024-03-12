18411197415c872bdb6986dd67c6d782e51e9e89
MoriaRoz <ruznurhv@gmail.com>
2024-03-12 18:34:22 +0200

    Select a date for the schedule

--- PL/StartDateWindow.xaml.cs
+++ PL/StartDateWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace PL
     /// <summary>
     /// Interaction logic for StartDateWindow.xaml
     /// </summary>
+    
     public partial class StartDateWindow : Window
     {
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
